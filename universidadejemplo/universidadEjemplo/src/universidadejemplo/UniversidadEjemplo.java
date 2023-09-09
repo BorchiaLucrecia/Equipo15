e8504e455deb45e47ba43cbe8d06ebce03a7ea5d
juanjosedichiara <juanjosedichiara@gmail.com>
2023-09-09 14:55:43 -0300

    Se estableció la Conexión con BD
    Hice una prueba en una tabla nueva llamada "Test" para verificar que la conexión funcione correctamente.

--- universidadejemplo/universidadEjemplo/src/universidadejemplo/UniversidadEjemplo.java
+++ universidadejemplo/universidadEjemplo/src/universidadejemplo/UniversidadEjemplo.java
@@ -1,11 +1,27 @@
 package universidadejemplo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import universidadejemplo.accesoADatos.Conexion;
 
 public class UniversidadEjemplo {
 
     public static void main(String[] args) {
+        //Se crea una nueva conexión utilizando la clase Conexion y el método getConexion()
         Conexion ulp = new Conexion("jdbc:mariadb://localhost/universidadulp", "root", "");
         ulp.getConexion();
+
+        //Inserción de PRUEBA EN LA CLASE TEST de la Base de Datos
+        try {
+            String insertar = "INSERT INTO test(nombre, estado) VALUES ('Juan Perez',true)";
+            Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/universidadulp", "root", "");
+            Statement sql = conexion.createStatement();
+            sql.executeUpdate(insertar);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al Conectar Base de Datos " + ex);
+        }
     }
 }
--- universidadejemplo/universidadEjemplo/src/universidadejemplo/accesoADatos/Conexion.java
+++ universidadejemplo/universidadEjemplo/src/universidadejemplo/accesoADatos/Conexion.java
@@ -3,6 +3,9 @@ package universidadejemplo.accesoADatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Conexion {
@@ -10,7 +13,7 @@ public class Conexion {
     private String url;
     private String usuario;
     private String pass;
-    private static Connection conexion = null; // es importante crear una variable de tipo connection
+    public static Connection conexion = null; // es importante crear una variable de tipo connection
 
     public Conexion(String url, String usuario, String pass) {
         this.usuario = usuario;
@@ -21,12 +24,16 @@ public class Conexion {
     public Connection getConexion() {
         if (conexion == null) {
             try {
-                Class.forName("org.mariadb.jdbc.Driver");
+                Class.forName("org.mariadb.jdbc.Driver").newInstance();
+                //Aqui se establece la conexion por el método estático getConnection(), retornando un objeto 
+                //de tipo Connection que se asigna a la variable conexion
                 conexion = DriverManager.getConnection(url, usuario, pass);
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar el Driver " + ex);
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "Error al Conectar a BD " + ex);
+            } catch (InstantiationException | IllegalAccessException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return conexion;
